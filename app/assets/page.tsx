2d0da4239fab360d6f1239f11fe0974c9a33494e
Krystian Kurlej <235661@student.uek.krakow.pl>
2025-11-24 23:20:55 +0100

    Dodaj komponent Drawer oraz funkcjonalność wyszukiwania do strony Assets; zaktualizuj plik ingredients.json o nowe składniki i zaktualizuj zależności w package.json

--- app/assets/page.tsx
+++ app/assets/page.tsx
@@ -1,7 +1,82 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerContent, DrawerHeader, DrawerTrigger, DrawerTitle, DrawerDescription } from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+import { CirclePlus, CircleX, Search } from "lucide-react";
+import React from "react";
+import ingredients from "@/db/ingredients.json";
+
 export default function AssetsPage() {
+  const [inputSearchTerm, setInputSearchTerm] = React.useState<string>("");
+  const [searchTerm, setSearchTerm] = React.useState<string>("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputSearchTerm(e.target.value);
+  };
+
+  const handleClearInputSearchTerm = () => {
+    setInputSearchTerm("");
+    setSearchTerm("");
+  };
+
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearchTerm(inputSearchTerm);
+  };
+
   return (
+    <>
     <header>
       <h1 className="text-4xl font-bold mt-8 mb-4">Your assets</h1>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button size="lg">
+            <CirclePlus />
+            Add New Asset
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent className="h-[85vh]">
+          <div className="w-full h-full flex flex-col">
+            <DrawerHeader>
+              <DrawerTitle className="hidden">Add New Asset</DrawerTitle>
+              <DrawerDescription>Add a new asset to your collection.</DrawerDescription>
+            </DrawerHeader>
+            <div className="px-4 flex flex-col flex-1 overflow-hidden">
+              <form onSubmit={handleSearchSubmit} className="relative mb-4">
+                <Search className="absolute top-1/2 left-3 -translate-y-1/2 w-5 h-5 text-stone-500" />
+                <Input
+                  placeholder="Search asset..."
+                  className="w-full h-12 px-10"
+                  value={inputSearchTerm}
+                  onChange={handleSearchChange}
+                  type="text"
+                />
+                <Button
+                  type="button"
+                  size="icon-lg"
+                  className={`absolute top-1/2 right-1 -translate-y-1/2 ${!inputSearchTerm && 'opacity-0 pointer-events-none'}`}
+                  variant="ghost"
+                  onClick={() => {handleClearInputSearchTerm()}}
+                >
+                  <CircleX className="w-5 h-5 text-stone-500" />
+                </Button>
+              </form>
+              <ul className="flex-1 overflow-y-auto pb-12">
+                {ingredients.map((ingredient) => (
+                  <li key={ingredient.id}>
+                    {ingredient.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </DrawerContent>
+      </Drawer>
     </header>
+    <main className="border-t mt-6">
+      <p className="py-8 text-center text-stone-500">No assets added yet.</p>
+    </main>
+    </>
   );
 }

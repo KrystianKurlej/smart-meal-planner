3dfe9aff0842d42bdafa57932bc1db9b9248254e
Krystian Kurlej <235661@student.uek.krakow.pl>
2025-11-25 16:02:08 +0100

    Zaktualizuj stronę rejestracji, aby obsługiwała rejestrację użytkowników za pomocą Firebase; popraw konfigurację Firebase.

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -3,71 +3,59 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { ArrowRight } from "lucide-react";
-import { Progress } from "@/components/ui/progress"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
 
 export default function RegisterPage() {
-    const [registerStep, setRegisterStep] = React.useState<number>(0);
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
-    const handleStepChange = (value: number) => {
-        setRegisterStep(value);
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
     }
 
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    }
+
+    const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("🚀 ~ handleRegister ~ email:", email)
+        console.log("🚀 ~ handleRegister ~ password:", password)
+
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            console.log("User registered:", userCredential.user);
+            setEmail("");
+            setPassword("");
+        } catch (error) {
+            console.error("Error registering user:", error);
+        }
+    };
+
     return (
-        <div className="flex flex-col h-screen relative bg-stone-100">
-            <header className="px-4 mb-8 mt-16">
-                <Progress value={Math.round((registerStep / 3) * 100)} />
-                <h2 className="text-3xl mt-8">
-                    {registerStep === 0 && "What's your email address?"}
-                    {registerStep === 1 && "Set a password"}
-                    {registerStep === 2 && "Confirm your password"}
-                    {registerStep === 3 && "You're all set!"}
-                </h2>
-            </header>
-            <main className="px-4 flex-1">
-                <form className="flex flex-col gap-4">
-                    <Input
-                        className={`w-full h-14 ${registerStep !== 0 ? 'hidden' : ''}`}
-                        type="email"
-                        placeholder="Email address"
-                        required
-                    />
-                    <Input
-                        className={`w-full h-14 ${registerStep !== 1 ? 'hidden' : ''}`}
-                        type="password"
-                        placeholder="Password"
-                        required
-                    />
-                    <Input
-                        className={`w-full h-14 ${registerStep !== 2 ? 'hidden' : ''}`}
-                        type="password"
-                        placeholder="Confirm Password"
-                        required
-                    />
-                </form>
-            </main>
-            <nav className="px-4 mb-8 flex justify-end">
-                {registerStep > 0 &&
-                    <Button
-                        type="button"
-                        size="icon-lg"
-                        variant="secondary"
-                        onClick={() => handleStepChange(registerStep - 1)}
-                        className="mr-4"
-                    >
-                        <ArrowRight className="rotate-180" />
-                    </Button>
-                }
-                {registerStep < 3 &&
-                    <Button
-                        type="button"
-                        size="icon-lg"
-                        onClick={() => handleStepChange(registerStep + 1)}
-                    >
-                        <ArrowRight />
-                    </Button>
-                }
-            </nav>
+        <div className="flex flex-col h-screen relative bg-stone-100 p-4">
+            <form onSubmit={handleRegister}>
+                <Input
+                    className="w-full h-14 mb-2"
+                    type="email"
+                    placeholder="Email address"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                />
+                <Input
+                    className="w-full h-14"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                />
+                <Button type="submit" className="w-full h-14 mt-4">
+                    Register
+                </Button>
+            </form>
         </div>
     )
 }
